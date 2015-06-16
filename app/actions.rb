3b154ee77fcd9899b8737f21f0adf5c4b4cc9995
dlgood <dlgood@goodpage.com>
2015-06-16 21:35:12 +0000

    Initial form setup

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,31 @@
 get '/' do
   erb :index
 end
+
+get '/songs' do
+  @songs = Song.all
+  erb :'songs/index'
+end
+
+get '/songs/new' do
+  @song = Song.new
+  erb :'songs/new'
+end
+
+post '/songs' do
+  @song = Song.new(
+    title: params[:title],
+    artist:  params[:artist],
+    url: params[:url]
+  )
+  if @song.save
+    redirect '/songs'
+  else
+    erb :'songs/new'
+  end
+end
+
+get '/songs/:id' do
+  @songs = Song.find params[:id]
+  erb :'songs/show'
+end
---
+++ app/models/song.rb
@@ -0,0 +1,5 @@
+class Song < ActiveRecord::Base
+
+  # validates :url, format: { with: URI.regexp }, if: Proc.new { |a| a.url.present? }
+
+end
